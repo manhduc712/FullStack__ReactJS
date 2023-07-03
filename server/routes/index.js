97e6d1a184b2a8b328303f266d6fde634407640d
Nguyen Manh Duc <duc96989@gmail.com>
2023-07-03 08:20:30 +0700

    done

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,11 @@
 const userRouter = require('./user');
 const { notFound, erroHandler } = require('../middlewares/erroHandler')
 const initRoutes = (app) => {
-    app.use('/api/user', userRouter);
     app.use('/api/user', userRouter)
-    app.use('/api/user', userRouter)
-
-    app.use(notFound);
-    app.use(erroHandler);
+    .use('/api/user', userRouter)
+    .use('/api/user', userRouter)
+    .use(notFound)
+    .use(erroHandler);
 }
 
 module.exports = initRoutes;
